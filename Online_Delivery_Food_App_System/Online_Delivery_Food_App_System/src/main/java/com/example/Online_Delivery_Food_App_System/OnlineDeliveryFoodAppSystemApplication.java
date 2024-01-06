4d19673a7175907d24af5c687ce27980a5a1ac74
vijaya vijju <118347362+vijayavallu@users.noreply.github.com>
2024-01-06 12:33:15 +0530

    Add files via upload

---
+++ Online_Delivery_Food_App_System/Online_Delivery_Food_App_System/src/main/java/com/example/Online_Delivery_Food_App_System/OnlineDeliveryFoodAppSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.Online_Delivery_Food_App_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineDeliveryFoodAppSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineDeliveryFoodAppSystemApplication.class, args);
+	}
+
+}
---
+++ Online_Delivery_Food_App_System/Online_Delivery_Food_App_System/src/test/java/com/example/Online_Delivery_Food_App_System/OnlineDeliveryFoodAppSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Online_Delivery_Food_App_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineDeliveryFoodAppSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
